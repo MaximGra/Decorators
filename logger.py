ce71eeaa8783f93d74b42194a176e008921f5b28
Grachev Maksim <magra85@gmail.com>
2022-04-02 19:09:04 +0300

    Added HW

---
+++ logger.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+
+def logger_path(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    def logger(old_function):
+        def new_function(*args, **kwargs):
+            result = old_function(*args)
+            with open(f'{path}/log.txt', 'a', encoding='utf8') as file:
+                file.write(
+                f'''\nВызвана функция {old_function.__name__}\nc аргументами {args}, результат {result}\nдата и время вызова {time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())}\n{"---"*20}''')
+            return result
+        return new_function
+    return logger
